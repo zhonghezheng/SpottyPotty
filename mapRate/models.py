cf95e5da2e49a4dc588dbc7d9a7caf8cae6d8afd
Nathan Ng <nathan@dhcp-v019-203.mobile.uci.edu>
2022-05-20 22:09:57 -0700

    updated database

--- mapRate/models.py
+++ mapRate/models.py
@@ -2,15 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Bathroom(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=255)
-    longitude = models.FloatField()
+    name = models.CharField(max_length=255, primary_key=True)
+    genders = models.CharField(max_length=1)
+    cleanliness = models.FloatField()
+    cleanlinessNo = models.IntegerField(default=0)
+    hygeine_products = models.FloatField()
+    hygeine_productsNo = models.IntegerField(default=0)
+    accessibility = models.FloatField()
+    accessibilityNo = models.IntegerField(default=0)
+    safety = models.FloatField()
+    safetyNo = models.IntegerField(default=0)
     latitude = models.FloatField()
-    rating = models.FloatField()
-    peopleRated = models.IntegerField()
+    longitude = models.FloatField()
+
 
     def __str__(self):
         return f"{self.name}: ({self.latitude}, {self.longitude}), {self.rating}*"
-
-class Permanent(models.Model):
-    cur_key = models.IntegerField()
